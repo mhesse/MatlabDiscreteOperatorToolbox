e3c02267fce5218c5606a5bd01b019cacb5fb9e4
Marc Hesse <mhesse@jsg.utexas.edu>
2023-03-19 20:15:03 -0500

    added face centers
    to avoid recomputing them everytime.

--- comp_cell_center_velocity.m
+++ comp_cell_center_velocity.m
@@ -1,17 +1,17 @@
-function [Vx_c,Vy_c] = comp_cell_center_velocity(v,Xc,Yc,Grid) % MDOT repo
+function [Vx_c,Vy_c] = comp_cell_center_velocity(v,Xc,Yc,Xx,Yx,Xy,Yy,Grid) % MDOT repo
 % author: Marc Hesse 
-% date: 29 May 2020
+% date: 29 May 2020, Mar 15, 2023
 % Description:
 % This functions averages the face velocities to the cell centers for a
 % standard tensor-product staggered mesh.
 
 % Interploate x-velocities
 Vx = reshape(v(1:Grid.Nfx),Grid.Ny,Grid.Nx+1);
-[Xx,Yx] = meshgrid(Grid.xf,Grid.yc);
+% [Xx,Yx] = meshgrid(Grid.xf,Grid.yc);
 Vx_c = interp2(Xx,Yx,Vx,Xc,Yc);
 
 % Interpolate y-velocities
 Vy = reshape(v(Grid.Nfx+1:end),Grid.Ny+1,Grid.Nx);
-[Xy,Yy] = meshgrid(Grid.xc,Grid.yf);
+% [Xy,Yy] = meshgrid(Grid.xc,Grid.yf);
 Vy_c = interp2(Xy,Yy,Vy,Xc,Yc);
 end
